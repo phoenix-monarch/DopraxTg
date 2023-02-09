e72d5522ca4eac10fad80a2c1e683aa997dd592a
lazygramyt <124551159+lazygramyt@users.noreply.github.com>
2023-02-09 06:31:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/lazygramyt/File-Sharing-Bot ok && cd ok && pip3 install -U -r requirements.txt
+python3 main.py
